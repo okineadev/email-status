2da786c05544dc9cff58fd711ee9a7cf6a712ea9
Okinea Dev <hi@okinea.dev>
2025-01-10 11:32:18 +0200

    upd

--- api/track.ts
+++ api/track.ts
@@ -1,15 +1,14 @@
-import { Request } from '@vercel/edge';
 
 export const config = { runtime: 'edge' };
 
 export default async (req: Request) => {
     let headersText = '';
-    // @ts-expect-error
     req.headers.forEach((value, key) => {
         headersText += `*${key}*: \`${value}\`\n`;
     });
 
     const currentTime = new Date().toString();
+    const query = new URL(req.url).searchParams;
 
     await fetch(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
         method: "POST",
@@ -18,7 +17,7 @@ export default async (req: Request) => {
         },
         body: JSON.stringify({
             chat_id: process.env.RECIPIENT_TELEGRAM_ID,
-            text: `New request at ${currentTime} !:\nID: Alif\n${headersText}`,
+            text: `New request at ${currentTime} !:\n\n${query.get('id') ? `ID: ${query.get('id')}\n` : ''}${headersText}`,
             parse_mode: "Markdown"
         })
     });
