608af121b6f1e9ccbf52552a2eefb80e298a3101
Okinea Dev <hi@okinea.dev>
2025-01-14 11:32:35 +0200

    perf: improve performance

--- api/track.ts
+++ api/track.ts
@@ -1,25 +1,44 @@
+import { waitUntil } from '@vercel/functions'
 
-export const config = { runtime: 'edge' };
+export const config = { runtime: 'edge' }
+
+/**
+ * Generates a formatted string representation of the given headers.
+ *
+ * @param headers - The Headers object containing the key-value pairs to be formatted.
+ * @returns A string where each header key-value pair is formatted as `*key*: \`value\``.
+ */
+function generateHeadersText(headers: Headers): string {
+	let headersText = ''
+	headers.forEach((value, key) => {
+		headersText += `*${key}*: \`${value}\`\n`
+	})
+
+	return headersText
+}
 
 export default async (req: Request) => {
-    let headersText = '';
-    req.headers.forEach((value, key) => {
-        headersText += `*${key}*: \`${value}\`\n`;
-    });
+	const currentTime = new Date().toString()
+	const query = new URL(req.url).searchParams
 
-    const currentTime = new Date().toString();
-    const query = new URL(req.url).searchParams;
+	waitUntil(
+		fetch(
+			`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					chat_id: process.env.RECIPIENT_TELEGRAM_ID,
+					text: `New request at ${currentTime} !:\n\n${query.get('id') ? `ID: ${query.get('id')}\n` : ''}${generateHeadersText(req.headers)}`,
+					parse_mode: 'Markdown',
+				}),
+			},
+		).catch((error) => {
+			console.error('Error sending message to Telegram:', error)
+		}),
+	)
 
-    await fetch(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            chat_id: process.env.RECIPIENT_TELEGRAM_ID,
-            text: `New request at ${currentTime} !:\n\n${query.get('id') ? `ID: ${query.get('id')}\n` : ''}${headersText}`,
-            parse_mode: "Markdown"
-        })
-    });
-    return new Response();
-};
+	return new Response()
+}
